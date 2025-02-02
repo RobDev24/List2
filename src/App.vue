ed955eaff79cb713be7b5c0d98f5234d33ff41a5
Robson A <robson_alves.18@hotmail.com>
2025-02-02 11:07:14 -0300

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,68 @@
 <template>
-  
-  
-  <div id="app">
-    <div class="navegação">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="nav">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </button>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+
+ <div id="nav">
+  <div class="navigation">
+
+    <nav>
+      <ul>
+
+        <li class="dropdown">
+          <a href="">Menu</a>
+
+          <div class="dropdown-menu">
+            <a href="">home</a>
+            <a href="">Seviços</a>
+            <a href="">Contato</a>
+            </div>
+
         </li>
       </ul>
-    </div>
-  </div>
-  </div>
-</nav>
-    </div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
     </nav>
-    <router-view/>
+
   </div>
+ </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.navegação{
-  background-color: black;
+nav{
+  background-color: blue;
 }
 
+nav li{
+display: inline-block;
+margin-left: 94%;
 
-
-nav {
-  padding: 30px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+nav li a{
+  display: inline-block;
+  color: aliceblue;
+  text-decoration: none;
+  padding: 15px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+nav li a:hover{
+  background-color: blueviolet;
 }
+
+.dropdown-menu{
+  position: absolute;
+  box-shadow: 0 0 2px black;
+  display: none;
+}
+.dropdown-menu a{
+  display: block;
+  color: black;
+  }
+
+  .dropdown:hover .dropdown-menu{
+    display: block;
+  }
 </style>
